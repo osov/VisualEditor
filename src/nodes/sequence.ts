08e3041a9ea4303b87911783bfcb97872e4178d7
dsa25 <73094994+dsa25@users.noreply.github.com>
2023-09-26 11:27:35 +0500

    codeReview node: sequence

--- src/nodes/sequence.ts
+++ src/nodes/sequence.ts
@@ -6,28 +6,34 @@ export class SequenceNode extends Classic.Node {
   width = 200
   height = 140
   private area: any;
+  private heightOut = 36;
 
   async makeOutputs(cnt: number) {
-    const count = Object.keys(this.outputs).length;
-
-    for (let i = 1; i <= count; i++) {
-      const indexOut = `o${i}`
+    for (let i = 1; i <= cnt; i++) {
+      const out = new Classic.Output(socket, `Out${i}`)
+      this.addOutput(`o${i}`, out)
+      this.height += this.heightOut
+    }
+    await this.area.update("node", this.id)
+  }
+  
+  async incrementOutput(cnt: number){
+    const out = new Classic.Output(socket, `Out${cnt}`)
+    this.addOutput(`o${cnt}`, out)
+    this.height += this.heightOut
+    await this.area.update("node", this.id)
+  }
+  
+  async decrementOutput(cnt: number){
+      const indexOut = `o${cnt}`
       // get id connection this output
       const itemCon = this.area.parent.connections.find((el: any) => el.source === this.id && el.sourceOutput === indexOut)
       // delete connection если есть
       if (itemCon)
         await this.area.removeConnectionView(itemCon.id)
       this.removeOutput(indexOut)
-      this.height -= 30
+      this.height -= this.heightOut
       await this.area.update("node", this.id)
-    }
-
-    for (let i = 1; i <= cnt; i++) {
-      const out = new Classic.Output(socket, `Out${i}`)
-      this.addOutput(`o${i}`, out)
-      this.height += 30
-      await this.area.update("node", this.id)
-    }
   }
 
   constructor(num_outputs = 2) {
@@ -41,13 +47,13 @@ export class SequenceNode extends Classic.Node {
       new TwoButtonControl("-", "+",
         async () => { // btn -
           if (num_outputs > 2) {
+            await this.decrementOutput(num_outputs);
             num_outputs--;
-            await this.makeOutputs(num_outputs);
           }
         },
         async () => {  // btn +
           num_outputs++;
-          await this.makeOutputs(num_outputs);
+          await this.incrementOutput(num_outputs);
         }
       )
     )
