05393c8a7708766c194f92ec671c73b75ace8ff6
Oleg <egsov@mail.ru>
2023-10-03 18:52:39 +0500

    controls help sergu =)

--- src/nodes/constants/boolean.ts
+++ src/nodes/constants/boolean.ts
@@ -3,39 +3,38 @@ import { socketBoolean } from '../../sockets'
 import { CheckboxControl } from "../../controls"
 
 export class BooleanNode
-    extends Classic.Node
-{
+    extends Classic.Node {
     width = 180;
     height = 140;
     private area: any;
     nodeTitle: { ru: string, type: string };
-    text: string = "Ложь";
-    active: boolean = false;
+    text: string = "Истина";
+    active = true;
 
     async toogleCheckbox() {
-        
+
         this.active = !this.active
-        this.text = this.text === "Истина" ? "Ложь" : "Истина"
+        this.text = !this.active ? "Ложь" : "Истина"
 
         // this.height += 20  // - это работает
 
-        console.log("click", {a: this.active, t: this.text});
-        await this.area.update("node", this.id)
+        console.log("click", { a: this.active, t: this.text }, this.id);
+        (this.controls as any)['Checkbox'].active = this.active;
+        (this.controls as any)['Checkbox'].text = this.text
+        await this.area.update("control", (this.controls as any)['Checkbox'].id);
+
     }
 
 
     constructor(initial: string) {
         super("Boolean");
-        this.nodeTitle = {ru: "Логическое", type: "yellow"}
+        this.nodeTitle = { ru: "Логическое", type: "yellow" }
         this.area = (window as any).area;
 
-        // this.toogleCheckbox()
-        console.log(initial);
-        
-        this.addControl("Checkbox", new CheckboxControl(this.text, this.active, async () => { await this.toogleCheckbox() } ))
+        this.addControl("Checkbox", new CheckboxControl(this.text, this.active, async () => await this.toogleCheckbox()))
 
         this.addOutput("out", new Classic.Output(socketBoolean, "логическое"))
-        // this.addControl("val", new Classic.InputControl("checkbox", { initial }));
+
     }
 
     serialize() {
