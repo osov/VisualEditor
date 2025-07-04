c80a4c5272ccd8e80aad86d8e27e7a829826cd8c
Oleg <egsov@mail.ru>
2025-07-04 09:25:52 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // или '0.0.0.0' для всех интерфейсов
+        port: 5173
+    },
     plugins: [
         vue(),
 
